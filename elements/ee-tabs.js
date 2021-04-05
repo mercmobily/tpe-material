5afd80ca6fcefd7e2dc7ef6c75ace6f838b86d61
freudFlintstone <raphael.mattos@contextaware.com.br>
2021-04-05 15:21:18 -0300

    Improved tabs

--- elements/ee-tabs.js
+++ elements/ee-tabs.js
@@ -6,10 +6,11 @@ export const EeTabs = (base) => {
         super.lit.css`
         :host {
           --ee-tabs-selected-color: var(--mat-primary-color);
+          --ee-tabs-active-color: var(--mat-primary-color);
           --ee-tabs-color: var(--mat-primary-text);
         }
 
-        :host nav > ::slotted(*:hover) {
+        :host nav > ::slotted(:hover) {
           box-shadow: var(--mat-theme-box-shadow4);
         }
 
@@ -20,20 +21,19 @@ export const EeTabs = (base) => {
           box-sizing: border-box;
         }
 
-        :host nav > ::slotted(*[active]) {
+        :host nav > ::slotted([active]) {
           color: var(--ee-tabs-selected-color);
           border-bottom: 0;
         }
 
-        :host nav > ::slotted(*:focus),
-        :host nav > ::slotted(*:hover) {
-          outline:0 ;
+        :host nav > ::slotted(:focus),
+        :host nav > ::slotted(:hover) {
+          outline: 0;
           border-bottom: 0;
-          filter: brightness(150%);
         }
 
         :host nav > ::slotted(*)::after,
-        :host nav > ::slotted(*:not([active]))::after {
+        :host nav > ::slotted(:not([active]))::after {
           content: '';
           position: absolute;
           transition: height 0.3s ease-in-out, left 0.3s ease-in-out, right 0.3s ease-in-out;
@@ -44,15 +44,15 @@ export const EeTabs = (base) => {
           background-color: var(--ee-tabs-selected-color);
         }
 
-        :host nav > ::slotted(*:focus)::after,
-        :host nav > ::slotted(*:hover)::after {
+        :host nav > ::slotted(:focus)::after,
+        :host nav > ::slotted(:hover)::after {
           height: 1px;
           left: 0.5px;
           right: 0.5px;
           transition: height 0.3s ease-in-out, left 0.3s ease-in-out, right 0.3s ease-in-out;
         }
 
-        :host nav > ::slotted(*[active])::after {
+        :host nav > ::slotted([active])::after {
           content: '';
           background-color: var(--ee-tabs-active-color);
           left: 0.5px;
@@ -62,7 +62,7 @@ export const EeTabs = (base) => {
           transition: height 0.3s ease-in-out, left 0.3s ease-in-out, right 0.3s ease-in-out;;
         }
 
-        :host nav > ::slotted(*:active) {
+        :host nav > ::slotted(:active) {
           background: #cccccc;
           border-bottom: 0;
           box-shadow: none;
